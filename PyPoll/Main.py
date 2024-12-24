5fdb7082dfcfb02636b82b6403c9621e5f0a019c
samoramachel50@gmail.com <ednahnyabengi@Ednahs-MacBook-Pro.local>
2024-12-23 19:56:43 -0800

    adding files

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -24,26 +24,34 @@ with open(file_to_load) as election_data:
     for row in reader:
         total_votes +=1
         candidate = row [2]
-
-    if candidate not in candidates: 
+        if candidate not in candidates: 
              candidates [candidate] = 0
-    candidates [candidate] += 1
-    results = []
-    for candidate, votes in candidates.items():
-        percentage = (votes/total_votes) * 100
-        results.append(f"{candidate}:{percentage:.3f}% ({votes})")
-        if votes > max_votes:
-             max_votes = votes
-             winner = candidate
+        candidates [candidate] += 1
+        results = []
+        for candidate, votes in candidates.items():
+            percentage = (votes/total_votes) * 100
+            results.append(f"{candidate}:{percentage:.3f}% ({votes})")
+            if votes > max_votes:
+               max_votes = votes
+               winner = candidate
+
+output_summary = (f"""
+Election Results
+.........................
+Total Votes: {total_votes}
+......................... \n""")
+out_summary_2 = ("\n".join(results))
+out_summary_3= (f"""
+.........................
+Winner : {winner}
+.........................""")
+print(output_summary,   out_summary_2, out_summary_3)
+with open(file_to_output, "w") as txt_file:
+    txt_file.write(output_summary)
+    txt_file.write(out_summary_2)
+    txt_file.write(out_summary_3)
 
 
-print("Election Results")  
-print(".........................")  
-print(f"Total Votes: {total_votes}")   
-print(".........................") 
-print("\n" .join(results))   
-print(".........................")
-print(f"Winner : {winner}")
-print(".........................")
+
 
 
